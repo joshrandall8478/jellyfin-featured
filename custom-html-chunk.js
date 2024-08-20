1cce3da3857a3ffe903b747543efbbca265d7452
Jellyfin <jellyfin@joshrandall.net>
2024-08-20 14:11:12 -0400

    updated custom-html-chunk

--- custom-html-chunk.js
+++ custom-html-chunk.js
@@ -1,11 +1,1 @@
-"use strict";
-(self.webpackChunk = self.webpackChunk || []).push([
-  [6368],
-  {
-    26964: function (a, e, t) {
-      t.r(e),
-        (e.default =
-          '<div id="indexPage" style="outline:0" data-role="page" data-dom-cache="true" class="page homePage libraryPage allLibraryPage backdropPage pageWithAbsoluteTabs withTabs" data-backdroptype="movie,series,book"> <style> .featurediframe { width: 89vw; height: 300px; display: block; margin: 0 auto; border-radius: 20px; border:4px solid #93c47d;} @media screen and (max-width: 960px) { .featurediframe{display: none;} } </style> <iframe class="featurediframe" src="./avatars/slideshow.html"></iframe> <div class="tabContent pageTabContent" id="homeTab" data-index="0"> <div class="sections"></div> </div> <div class="tabContent pageTabContent" id="favoritesTab" data-index="1"> <div class="sections"></div> </div> </div> ');
-    },
-  },
-]);
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[8372],{5939:function(a,e,t){t.r(e),e.default='<div id="indexPage" style="outline:0" data-role="page" data-dom-cache="true" class="page homePage libraryPage allLibraryPage backdropPage pageWithAbsoluteTabs withTabs" data-backdroptype="movie,series,book"> <style> .featurediframe { width: 89vw; height: 300px; display: block; margin: 0 auto; border-radius: 20px; border:4px solid #93c47d;} @media screen and (max-width: 960px) { .featurediframe{display: none;} } </style> <iframe class="featurediframe" src="./avatars/slideshow.html"></iframe> <div class="tabContent pageTabContent" id="homeTab" data-index="0"> <div class="sections"></div> </div> <div class="tabContent pageTabContent" id="favoritesTab" data-index="1"> <div class="sections"></div> </div> </div> '}}]);
