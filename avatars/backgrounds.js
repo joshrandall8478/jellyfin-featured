22ed4eb9c9eeb3b7efdbab0fac5cd9618e4ff994
Joshua Randall <josh@joshrandall.net>
2026-04-16 21:58:08 -0400

    updated banner

--- avatars/backgrounds.js
+++ avatars/backgrounds.js
@@ -1,21 +1,11 @@
 // jjk-s3
-let folder = "jjks3/";
-let limit = 4;
+let folder = "reze/";
+let limit = 8;
 const format = "jpg";
 
 let mainRandom = Math.floor(Math.random() * limit) + 1;
 
 let image = folder + mainRandom + "." + format;
 
-document.getElementById("jjks3").src = image;
-
-// Reze
-folder = "reze/";
-limit = 8;
-
-
-mainRandom = Math.floor(Math.random() * limit) + 1;
-
-image = folder + mainRandom + "." + format;
 
 document.getElementById("reze").src = image;
